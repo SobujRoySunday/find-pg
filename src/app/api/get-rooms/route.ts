28efca5382464fa3779b5aa505e991afd4509322
Sorbopriyo Roy <sorbopriyo@gmail.com>
2023-12-03 15:34:43 +0530

    code refactor

--- src/app/api/get-rooms/route.ts
+++ src/app/api/get-rooms/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     let rooms = await prisma.rooms.findMany()
     if (!rooms) {
